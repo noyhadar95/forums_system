37b1ea660a4dc16347a46adfcc2467c944a0e498
shush0606 <shush059@walla.com>
2016-04-02 17:24:51 +0300

    Interface

---
+++ ForumsSystem/ForumsSystem/Server/ForumManagement/DomainLayer/Forum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumsSystem.Server.ForumManagement.DomainLayer
+{
+    class Forum : IForum
+    {
+
+        public void EditForumProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterToForum(string userName, string password, string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateSubForum(string subForumName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ForumsSystem/ForumsSystem/Server/ForumManagement/DomainLayer/IForum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumsSystem.Server.ForumManagement.DomainLayer
+{
+    interface IForum
+    {
+        public void EditForumProperties();
+
+        public bool RegisterToForum(string userName, string password, String Email);
+
+        public bool CreateSubForum(string subForumName);
+
+        public bool Login(string userName, string password);
+    }
+}
