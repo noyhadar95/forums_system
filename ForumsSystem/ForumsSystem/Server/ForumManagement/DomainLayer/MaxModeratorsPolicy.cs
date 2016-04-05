286b86fc492d8f29accb1dffa111243b0b76ce13
omerhay95 <omerhay95@gmail.com>
2016-04-05 17:10:19 +0300

    added max moderator policy

---
+++ ForumsSystem/ForumsSystem/Server/ForumManagement/DomainLayer/MaxModeratorsPolicy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumsSystem.Server.ForumManagement.DomainLayer
+{
+
+    class MaxModeratorsPolicy:Policy
+    {
+        private int maxModerators;
+
+        public MaxModeratorsPolicy(Policies type, int maxModerators):base(type)
+        {
+            this.maxModerators = maxModerators;
+        }
+
+        /// <summary>
+        /// check if a there is a free slot for a moderator 
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public override bool CheckPolicy(PolicyParametersObject param)
+        {
+            if (param.GetPolicy() == type)
+            {
+
+
+                return this.maxModerators > param.NumOfModerators;
+            }
+            else
+                return base.CheckPolicy(param);
+
+        }
+    }
+}
