6bbd14e0e7ef4258c3cac98d8a0317878687fefb
shush0606 <shush059@walla.com>
2016-04-07 14:58:41 +0300

    system and super admin

---
+++ ForumsSystem/ForumsSystem/Server/ForumManagement/DomainLayer/SuperAdmin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumsSystem.Server.ForumManagement.DomainLayer
+{
+    class SuperAdmin
+    {
+       
+
+        public string userName { get;  set; }
+        public string password { get; set; }
+        public System forumSystem { get; private set; }
+
+        public SuperAdmin(string userName, string password, System forumSystem)
+        {
+            this.userName = userName;
+            this.password = password;
+            this.forumSystem = forumSystem;
+        }
+
+        public Forum createForum(string forumName)
+        {
+            return this.forumSystem.createForum(forumName);
+        }
+        public void removeForum(string forumName)
+        {
+            this.forumSystem.removeForum(forumName);
+        }
+    }
+}
