aeed76599b5aa4eec98cbf97c98a4265f6d2f62e
shush0606 <shush059@walla.com>
2016-05-04 20:43:51 +0300

    omer destroya

---
+++ ForumsSystem/UnitTests/ServerUnitTests/Data Access Layer/ErrorLogTests.cs
@@ -0,0 +1,36 @@
+﻿﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForumsSystem.Server.ForumManagement.Data_Access_Layer;
+
+namespace UnitTests.ServerUnitTests.Data_Access_Layer
+{
+    [TestClass]
+    public class ErrorLogTests
+    {
+        DAL_ErrorLogs de;
+        [TestInitialize()]
+        public void Initialize()
+        {
+            de = new DAL_ErrorLogs();
+        }
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            de = null;
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            DateTime now = DateTime.Now;
+            DateTime next = now.AddMinutes(10);
+            de.AddLog(now, "test1");
+            de.AddLog(next, "test2");
+            var d = de.GetLogByDate(next);
+            Assert.IsTrue(d.Rows.Count >= 2);
+            de.DeleteLog(now);
+            de.DeleteLog(next);
+
+        }
+    }
+}
