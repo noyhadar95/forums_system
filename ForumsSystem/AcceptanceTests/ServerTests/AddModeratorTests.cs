c365bc595df255d05bf8afffffeb4e62dc767bb4
omerhay95 <omerhay95@gmail.com>
2016-05-08 20:44:09 +0300

    a

--- ForumsSystem/AcceptanceTests/ServerTests/AddModeratorTests.cs
+++ ForumsSystem/AcceptanceTests/ServerTests/AddModeratorTests.cs
@@ -21,6 +21,10 @@ namespace AcceptanceTests.ServerTests
         [TestMethod]
         public void TestAddModerator()
         {
+           /* ForumsSystem.Server.ForumManagement.Data_Access_Layer.DAL_Forum d = new ForumsSystem.Server.ForumManagement.Data_Access_Layer.DAL_Forum();
+            d.DeleteAll();
+            Assert.Fail();
+            */
             string forumName = GetNextForum();
             PoliciesStub forumPolicy = PoliciesStub.Password;
             string username1 = "user1";
