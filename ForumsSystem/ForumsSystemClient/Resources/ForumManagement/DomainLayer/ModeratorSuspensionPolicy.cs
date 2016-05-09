c40cf0c02e8569a2b94634d7ad7ad0fe12731266
nimrodharris <harris_nimrod@hotmail.com>
2016-05-09 16:19:40 +0300

    policys fix

--- ForumsSystem/ForumsSystemClient/Resources/ForumManagement/DomainLayer/ModeratorSuspensionPolicy.cs
+++ ForumsSystem/ForumsSystemClient/Resources/ForumManagement/DomainLayer/ModeratorSuspensionPolicy.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace ForumsSystemClient.Resources.ForumManagement.DomainLayer
 {
+    [DataContract(IsReference = true)]
     public class ModeratorSuspensionPolicy :Policy
     {
+        [DataMember]
         private int numOfComplaints;
 
         public ModeratorSuspensionPolicy() : base()
--- ForumsSystem/ForumsSystemClient/Resources/ForumManagement/DomainLayer/PasswordPolicy.cs
+++ ForumsSystem/ForumsSystemClient/Resources/ForumManagement/DomainLayer/PasswordPolicy.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace ForumsSystemClient.Resources.ForumManagement.DomainLayer
 {
+
+    [DataContract(IsReference = true)]
     public class PasswordPolicy :Policy
     {
+        [DataMember]
         private int requiredLength;
+        [DataMember]
         private int passwordValidity;
 
         public int RequiredLength
